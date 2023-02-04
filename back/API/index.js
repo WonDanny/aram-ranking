2ef231a4e12cb1ecca94dba0bb064d732d2579d3
dannywon <tmdeofjrzl1@naver.com>
2023-02-04 17:50:43 +0900

    test3

--- back/API/index.js
+++ back/API/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = normalizePort(process.env.PORT || '3001');;
+const port = process.env.PORT || '3001'
 
 app.get('/test', (req, res) => {
     res.send('Hello world');
